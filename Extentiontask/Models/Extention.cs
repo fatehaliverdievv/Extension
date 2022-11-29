62b6f8885b2fbf8740031e13bba7c5daab6c3d66
fateh.aliverdievv <tu7l7qsfn@code.edu.az>
2022-11-29 20:43:29 +0400

    30novembertaskdone

---
+++ Extentiontask/Models/Extention.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extentiontask.Models
+{
+    static class Extention
+    {
+        public static bool HasDigit(this string str)
+        {
+            bool answerr = false;
+            str = str.Trim();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]))
+                {
+                    answerr = true;
+                }
+            }
+            return answerr;
+        }
+        public static bool IsOdd(this int num)
+        {
+            if (num % 2 == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool IsEven(this int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static void ToCapitalize(this string str)
+        {
+            str = str.Trim();
+            if (String.IsNullOrEmpty(str))
+            {
+                Console.WriteLine("Empty String");
+            }
+            else
+            {
+                Console.WriteLine(char.ToUpper(str[0])+str.Substring(1).ToLower());
+             }
+        }
+
+    }
+}
---
+++ Extentiontask/Program.cs
@@ -0,0 +1,19 @@
+﻿using Extentiontask.Models;
+
+namespace Extentiontask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string : ");
+            string str=Console.ReadLine();
+            str.ToCapitalize();
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write("number is odd : ");
+            Console.WriteLine(num.IsOdd());
+            Console.Write("number is even : ");
+            Console.WriteLine(num.IsEven());
+        }
+    }
+}
